e93229e98cb1bbcbec756200162db6812cb931ca
kazuma_kurita <kazuma.kurita@gmail.com>
2020-07-28 14:39:24 +0900

    Add Delete Function

--- blog/views.py
+++ blog/views.py
@@ -40,3 +40,8 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
